6046eaa16e4e4ec94505ad5fc0ceb3baa744d5ec
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-09-22 00:36:14 -0700

    Create circle.js

---
+++ hw/hw3/circle.js
@@ -0,0 +1,36 @@
+background(255)
+var location = 0;
+var direction = 0;
+
+
+while(direction == 0){
+	background(255);
+  fill(255);
+	ellipse(100+location,100,20,20);
+	location = location + 10;
+  if(location>=240){
+  	direction = 1-direction;
+  }
+}
+
+
+location = 0;
+while(direction == 1){
+  background(255);
+  fill(255);
+  ellipse(350-location,100,20,20);
+  location = location + 10;
+  if(location>340){
+  	direction = 1-direction;
+  }
+}
+
+while(direction == 0){
+	background(255);
+  fill(255);
+	ellipse(100+location,100,20,20);
+	location = location + 10;
+  if(location>=240){
+  	direction = 1-direction;
+  }
+}
