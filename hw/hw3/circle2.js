486f52b002d087bef83880803c9731b8ad679e49
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-09-27 08:14:30 -0700

    Update circle2.js

--- hw/hw3/circle2.js
+++ hw/hw3/circle2.js
@@ -5,33 +5,33 @@ var direction = 0;
 
 while(direction == 0){
 	background(255);
-  fill(255);
+  	fill(255);
 	ellipse(100+location,100,20,20);
 	location = location + 10;
-  if(location>=240){
-  	direction = 1-direction;
-  }
+  	if(location>=240){
+  		direction = 1-direction;
+ 	 }
 }
 
 
 location = 0;
 while(direction == 1){
-  background(255);
-  fill(255);
-  ellipse(350-location,100,20,20);
-  location = location + 10;
-  if(location>340){
-  	direction = 1-direction;
-  }
+  	background(255);
+	fill(255);
+ 	ellipse(350-location,100,20,20);
+ 	location = location + 10;
+ 	if(location>340){
+  		direction = 1-direction;
+  	}
 }
 
 location = 0;
 while(direction == 0){
 	background(255);
-  fill(255);
+  	fill(255);
 	ellipse(location,100,20,20);
 	location = location + 10;
-  if(location>=240){
-  	direction = 1-direction;
-  }
+  	if(location>=240){
+  		direction = 1-direction;
+ 	}
 }
