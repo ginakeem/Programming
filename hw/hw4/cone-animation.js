d7947e70d0a9a2a75fc9dbd0742971896c70627a
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-04 08:58:20 -0700

    Update cone-animation.js

--- hw/hw4/cone-animation.js
+++ hw/hw4/cone-animation.js
@@ -16,3 +16,20 @@ function draw() {
 	}
 	
 }
+
+
+//correct answer
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+	
+	var x=10;
+	while(x<width){
+		line(mouseX,mouseY,x,10);
+		x=x+10;
+	}
+}
