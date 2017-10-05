8175b54e7fe23bfaf9cd50dc008dc890924120f5
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-05 14:15:53 -0700

    Create lewitt-295.js

---
+++ hw/hw5/lewitt-295.js
@@ -0,0 +1,20 @@
+//295
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(0);
+	stroke(255);
+	fill(0);
+	rect(20,20,360,360);
+	ellipse(200,200,360,360);
+	line(200,20,20,380)
+	line(200,20,380,380);
+	line(20,380,100,20);
+	line(100,20,100,380);
+	line(380,380,300,20);
+	line(300,20,300,380);
+	line(300,380,380,20);
+}
