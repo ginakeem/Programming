a7173dd905b2897530ae505c03756a7f159c80d9
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-05 14:05:53 -0700

    Create lewitt-160.js

---
+++ hw/hw5/lewitt-160.js
@@ -0,0 +1,14 @@
+//160
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(255);
+	stroke(0);
+	rect(20,20,360,360);
+	stroke(255,0,0);
+	line(50,50,350,350);
+	line(350,50,50,350);
+}
