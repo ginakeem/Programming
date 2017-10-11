ad485248ecb3510873d07130ceacffedb8de219f
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-11 08:57:43 -0700

    Update circles-4.js

--- hw/hw5/circles-4.js
+++ hw/hw5/circles-4.js
@@ -1,14 +1,17 @@
+
 var sizes = [4, 9, 10, 5, 112, 95, 40, 38];
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 600);
 	colorMode(HSB);
-
   noStroke();
-    
-  for (var i = 0; i < sizes.length; i += 1) {
+}
+
+function draw(){
+	background(255);
+	for (var i = 0; i < sizes.length; i += 1) {
+		var h = height/2+random(-2,2);
     fill(sizes[i],100,90);
-    ellipse(30+i*80, height/2, sizes[i], sizes[i]);
-  }
-	
+    ellipse(30+i*80, h, sizes[i], sizes[i]);
+	}
 }
