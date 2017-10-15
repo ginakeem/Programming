a23abbef8c99b8a4bad75aecb97e32a9652a7660
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-15 03:35:23 -0700

    Create stella-2.js

---
+++ hw/hw6/stella-2.js
@@ -0,0 +1,24 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+	background(255);
+	var x=0;
+	var y=0;
+	var w=400;
+	var h=400;
+	var color=0;
+	
+	while(x<400){
+		strokeWeight(10);
+		stroke(color);
+		noFill();
+		rect(x,y,w,h);
+		x+=20;
+		y+=20;
+		w-=40;
+		h-=40;
+		color+=10;
+	}
+}
