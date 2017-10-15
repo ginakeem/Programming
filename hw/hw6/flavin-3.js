20b6a933c3e39c6eefc650b6df714557175a1f4f
ginakeem <31739565+ginakeem@users.noreply.github.com>
2017-10-15 02:26:56 -0700

    Create flavin-3.js

---
+++ hw/hw6/flavin-3.js
@@ -0,0 +1,21 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+	background(255);
+	
+	stroke(0);
+	strokeWeight(10);
+	line(190,400,190,100);
+	line(210,400,210,100);
+	
+	line(170,400,170,125);
+	line(230,400,230,125);
+	
+	line(150,400,150,220);
+	line(250,400,250,220);
+	
+	line(130,400,130,350);
+	line(270,400,270,350);
+}
